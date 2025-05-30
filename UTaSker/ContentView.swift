5527d1709238db65ef47f57c821b5aa6fd8ab496
Ashton <ashton.king@student.uts.edu.au>
2025-05-30 13:41:26 +1000

    Testing again!

--- UTaSker/ContentView.swift
+++ UTaSker/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("This could be our home view!! Testing!")
+            Text("This could be our home view!!")
         }
         .padding()
     }
