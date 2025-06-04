aae85c2bcf7ce1d4ea64f1a462320f295d6e7a2f
Ashton <ashton.king@student.uts.edu.au>
2025-06-04 12:43:00 +1000

    SignIn - Formatting of inputs and checks to prevent user from selecting sign in button with invalid (dummy) entries in the text field

--- UTaSker/Views/Landing/SignInView.swift
+++ UTaSker/Views/Landing/SignInView.swift
@@ -7,10 +7,16 @@
 
 import SwiftUI
 
+private enum Field {
+    case email, password
+}
 struct SignInView: View {
+    
     @State var email: String = ""
     @State var password: String = ""
-    @State var validSignIn: Bool = false
+    @State var signInDisabled: Bool = true
+    @FocusState private var focusField: Field?
+    
     var body: some View {
         NavigationStack {
             VStack {
@@ -22,35 +28,51 @@ struct SignInView: View {
                     Image("UTaskerLogo")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 325)
+                        //.frame(width: 325)
                     
                     VStack(alignment: .leading) {
                         Text("UTS Email Address")
                         TextField("\("firstname.lastname@student.uts.edu.au")", text: $email)
+                            .textFieldStyle(.roundedBorder)
+                            .keyboardType(.emailAddress)
+                            .autocorrectionDisabled()
+                            .textInputAutocapitalization(.never)
+                            .submitLabel(.next)
+                            
                             .padding(6)
-                            .background(
-                                RoundedRectangle(cornerRadius: 5)
-                                    .foregroundStyle(.box)
-                            )
+                            .focused($focusField, equals: .email)
+                            .onSubmit {
+                                focusField = .password
+                            }
+                            .onChange(of: email) { oldValue, newValue in
+                                CheckValidSignIn()
+                            }
                     }
                     .padding(.bottom)
                     
                     VStack(alignment: .leading) {
                         Text("Password")
                         SecureField("Minimum 8 characters", text: $password)
+                            .textFieldStyle(.roundedBorder)
+                            .submitLabel(.done)
                             .padding(6)
-                            .background(
-                                RoundedRectangle(cornerRadius: 5)
-                                    .foregroundStyle(.box)
-                            )
+                            .focused($focusField, equals: .password)
+                            .onSubmit {
+                                focusField = nil
+                            }
+                            .onChange(of: password) {oldValue, newValue in
+                            CheckValidSignIn()
+                            }
                     }
                     .padding(.bottom)
+                    
                     Spacer()
                     PrimaryButton(
                         title: "Sign In",
                         action: { print("WAHA") },
-                        backgroundColor: .blue,
-                        textColor: .white
+                        //backgroundColor: .blue,
+                        //textColor: .white
+                        isDisabled: signInDisabled
                     )
                 }
                 .padding()
@@ -58,7 +80,17 @@ struct SignInView: View {
             .padding()
         }
     }
+    
+    func CheckValidSignIn() {
+        //TODO: Makes these proper checks for existing emails and passwords.
+        let goodEmail: Bool = email.count > 6 && email.contains("@")
+        let goodPassword: Bool = password.count >= 8
+        signInDisabled = !(goodEmail && goodPassword)
+    }
+    
 }
+
+
 
 #Preview {
     SignInView()
